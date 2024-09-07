7c83c76176b56716fe21042a26ff3a7a86c4c241
Kristina Ryzhova <zelbusinka@yandex.ru>
2024-09-07 21:19:59 +0300

    ingredients modal open

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectorIngredients } from '../../services/slices/ingredientsSlice/ingredientsSlice';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const ingredients = useSelector(selectorIngredients);
+  const { id } = useParams();
+  const ingredientData = ingredients.find((item) => item._id === id);
 
   if (!ingredientData) {
     return <Preloader />;
